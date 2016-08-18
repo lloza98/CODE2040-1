ce4a7e3f1a8e86f3cc9bda71ba520b30cdce197b
Joseph <jlikhuva@stanford.edu>
2016-08-17 19:11:03 -0700

    code re-use and cleanup

--- prefix.py
+++ prefix.py
@@ -1,34 +1,29 @@
-#
-#
-#
-#
-#
-#
+# This script JSON data made up of a string prefix and a collection
+# of random strings. The program implements routines that create a 
+# new collection made up of strings that do not start with the given
+# string prefix
 
 import requests
 import json
-from needle_in_haystack import get_exercise
+from needle_in_haystack import get_exercise, post_exercise
+from registration import token, doubleCheck
 
-token = '0756140fa1f238700cd260f5f813fab6'
+# instance variables
 get_url = 'http://challenge.code2040.org/api/prefix'
 post_url = 'http://challenge.code2040.org/api/prefix/validate'
 
+# create the json object using the data send by the remote server
 def get_json_obj(token, get_url):
     stringResponse = get_exercise(get_url, token).text
     return json.loads(stringResponse)
 
+# Get the prefix and the string collection
 jsonResponse = get_json_obj(token, get_url)
-
-print jsonResponse
-
 prefix = jsonResponse['prefix']
 r_array  = jsonResponse['array']
 
-'''
-print prefix
-print r_array
-'''
-
+# create a new collection made up of strings that do
+# not start with prefix
 def make_new_array(prefix, array):
     new_list = []
     for each in array:
@@ -36,18 +31,7 @@ def make_new_array(prefix, array):
             new_list.append(each)
     return new_list
 
-
 n_array = make_new_array(prefix, r_array)
-#print n_array
 
-data = {
-    'token' : token,
-    'array': n_array
-    }
-
-def post():
-    return requests.post(post_url, json=data)
-
-r = post()
-print r.status_code
-print r.text
+post = post_exercise(n_array, post_url, 'array')
+doubleCheck(post)
